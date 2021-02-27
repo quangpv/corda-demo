b292b584f856fc98308c7ead5f1cb621235909ea
quangpv <quangpv.uit@gmail.com>
2021-02-27 18:46:50 +0700

    [update] run deploy

--- runDeploy.sh
+++ runDeploy.sh
@@ -1,2 +1,7 @@
 ./gradlew deployNodes
 ./build/nodes/runnodes
+
+./gradlew --build-file clients/build.gradle runServerBankA
+echo "Bank A available with port 10006"
+#./gradlew --build-file clients/build.gradle runServerBankB
+#./gradlew --build-file clients/build.gradle runServerBankC
